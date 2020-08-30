6ea09d52df2d4a8d516acf6d97ea85895f28d550
Marek Szuba <mszuba01@gmail.com>
2020-08-30 22:46:31 +0200

    create bg-animation in react.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Animated from './Animated';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="balloon-animation">
+          <Animated
+            className="flying-cloud"
+            src='./cloud.png'
+            ratioX='0.2'
+            ratioY='0.23'
+          />
+          <Animated
+            className="flying-cloud"
+            src='./cloud.png'
+            ratioX='0.26'
+            ratioY='0.34'
+          />
+          <Animated
+            className="flying-cloud"
+            src='./cloud.png'
+            ratioX='0.1'
+            ratioY='0.11'
+          />
+          <Animated
+            className="flying-cloud"
+            src='./cloud.png'
+            ratioX='0.13'
+            ratioY='0.53'
+          />
+          <Animated
+            className="flying-cloud"
+            src='./cloud.png'
+            ratioX='0.23'
+            ratioY='0.41'
+          />
+          <Animated
+            className="flying-cloud"
+            src='./cloud.png'
+            ratioX='0.18'
+            ratioY='0.28'
+          />
+          <Animated
+            className="flying-balloon"
+            src='./airballoon.png'
+            ratioX='0.07'
+            ratioY='0.3'
+          />
+
+        </div>
       </header>
     </div>
   );
